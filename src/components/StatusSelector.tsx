551844eac038194a88438db0f04fcc3e93c2d570
denton21 <lugath21@gmail.com>
2026-01-11 13:53:51 +0400

    Update StatusSelector.tsx

--- src/components/StatusSelector.tsx
+++ src/components/StatusSelector.tsx
@@ -190,7 +190,10 @@ export default function StatusSelector({
                 </div>
                 <button
                     type="button"
-                    onClick={() => setIsPrivate(!isPrivate)}
+                    onClick={() => {
+                        console.log('Toggle clicked, current isPrivate:', isPrivate, '-> setting to:', !isPrivate);
+                        setIsPrivate(!isPrivate);
+                    }}
                     className={`relative w-12 h-6 rounded-full transition-colors ${isPrivate ? 'bg-purple-500' : 'bg-white/20'}`}
                 >
                     <span className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full transition-transform ${isPrivate ? 'translate-x-6' : ''}`} />
